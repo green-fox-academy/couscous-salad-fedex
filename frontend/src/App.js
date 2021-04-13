595111568cb96ac520a2246d469d0ec167bb8550
Deutsch Gábor <deutschgabor92@gmail.com>
2021-04-13 13:26:41 +0200

    NotFound page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import './App.css';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/">
-        </Route>
+        <Route path="/"></Route>
         <Switch>
           <Route path="/login">
             <Login />
           </Route>
           <Route path="/register">
             <Register />
+          </Route>
+          <Route path="*">
+            <NotFound />
           </Route>
         </Switch>
       </div>
