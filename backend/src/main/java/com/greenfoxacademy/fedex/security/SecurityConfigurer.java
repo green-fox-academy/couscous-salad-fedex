a55395bc60bf8b792871789cd94834bcba1c8617
temodin <solyom.balint@gmail.com>
2021-04-15 17:54:13 +0200

    authorization for token header

--- backend/src/main/java/com/greenfoxacademy/fedex/security/SecurityConfigurer.java
+++ backend/src/main/java/com/greenfoxacademy/fedex/security/SecurityConfigurer.java
@@ -29,7 +29,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().antMatchers("/login","/register").permitAll()
+    http.cors().and().csrf().disable().authorizeRequests().antMatchers("/login","/register").permitAll()
         .anyRequest().authenticated()
         .and().sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
